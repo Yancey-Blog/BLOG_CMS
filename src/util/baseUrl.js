25996099e3492c9b9d1fbb4bb34235db5aa7dabf
yanceyleo <developer@yanceyleo.com>
2018-10-21 00:29:49 +0800

    build

--- src/util/baseUrl.js
+++ src/util/baseUrl.js
@@ -1,6 +1,7 @@
 const baseUri = {
   development: 'http://127.0.0.1:3001/api/',
-  production: 'https://api.yanceyleo.com/api/',
+  // production: 'https://api.yanceyleo.com/api/',
+  production: 'http://127.0.0.1:3001/api/',
 };
 
 export default baseUri;
